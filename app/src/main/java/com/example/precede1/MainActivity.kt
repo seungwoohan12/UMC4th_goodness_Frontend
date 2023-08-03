479313d95a276b10242a6bb7b0c4cc9c21344cc2
11yubin <11yubine@gmail.com>
2023-08-03 17:42:58 +0900

    Second Commit

--- app/src/main/java/com/example/precede1/MainActivity.kt
+++ app/src/main/java/com/example/precede1/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.precede1
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var stab_btn : Button = findViewById(R.id.btn_organ)
+        stab_btn.setOnClickListener {
+            var Intent = Intent(this, LoginActivity::class.java)
+
+            startActivity(Intent)
+        }
     }
 }
